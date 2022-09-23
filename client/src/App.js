e6ca50d203ac74f82c07bce4311576d6399e9e03
Arne Smebye <arne.smebye@tieto.com>
2022-09-23 08:56:51 +0200

    copy from udemy

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Udemy version of the complex project
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
